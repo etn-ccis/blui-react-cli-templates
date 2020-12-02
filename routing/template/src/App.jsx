2400f148090a136c6fe3e3ffffb53748300672af
joebochill <boyle.p.joseph@gmail.com>
2020-12-02 13:11:39 -0500

    Add javascript routing template

---
+++ routing/template/src/App.jsx
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+import { DrawerContext } from './contexts/drawerContextProvider';
+import { NavigationDrawer } from './router/drawer';
+import { MainRouter } from './router/main';
+import { DrawerLayout } from '@pxblue/react-components';
+
+export const App = () => {
+    const [drawerOpen, setDrawerOpen] = useState(false);
+
+    return (
+        <DrawerContext.Provider
+            value={{
+                drawerOpen,
+                setDrawerOpen,
+            }}
+        >
+            <DrawerLayout drawer={<NavigationDrawer />} style={{ height: '100%' }}>
+                <MainRouter />
+            </DrawerLayout>
+        </DrawerContext.Provider>
+    );
+};
---
+++ routing/template/src/index.jsx
@@ -0,0 +1,28 @@
+import 'react-app-polyfill/ie11';
+import 'react-app-polyfill/stable';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { CssBaseline, ThemeProvider, createMuiTheme } from '@material-ui/core';
+import { BrowserRouter } from 'react-router-dom';
+import * as PXBThemes from '@pxblue/react-themes';
+import 'typeface-open-sans';
+import { App } from './App';
+import reportWebVitals from './reportWebVitals';
+import './index.css';
+
+ReactDOM.render(
+    <React.StrictMode>
+        <ThemeProvider theme={createMuiTheme(PXBThemes.blue)}>
+            <BrowserRouter>
+                <CssBaseline />
+                <App />
+            </BrowserRouter>
+        </ThemeProvider>
+    </React.StrictMode>,
+    document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
