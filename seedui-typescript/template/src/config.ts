4d14cdd5ea63fbbaae81d8bc6df2da1a4b50d997
vigneshranganathan-eaton <vigneshranganathan@eaton.com>
2023-08-22 01:42:34 +0530

    resolve conflicts

--- seedui-typescript/template/src/config.ts
+++ seedui-typescript/template/src/config.ts
@@ -1,5 +1,7 @@
 export const authConfig = {
 	adopterId: "${ADOPTER_ID}",   
 	adopterApplicationName: "${ADOPTER_APPLICATION_NAME}",             
-	applicationId: "${APPLICATION_ID}"
+	applicationId: "${APPLICATION_ID}",
+	inviteLimitation: "${INVITE_LIMITATION}",  // true or false
+    inviteCount: "${INVITE_COUNT}"  // numbers
 }
