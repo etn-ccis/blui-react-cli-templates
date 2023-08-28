f8675b9aac53b1298d832c685bf0eb820e41ea03
vigneshranganathan-eaton <vigneshranganathan@eaton.com>
2023-08-28 16:58:29 +0530

    remove limitation property

--- seedui-typescript/template/src/config.ts
+++ seedui-typescript/template/src/config.ts
@@ -2,6 +2,5 @@ export const authConfig = {
 	adopterId: "${ADOPTER_ID}",   
 	adopterApplicationName: "${ADOPTER_APPLICATION_NAME}",             
 	applicationId: "${APPLICATION_ID}",
-	inviteLimitation: "${INVITE_LIMITATION}",  // true or false
     inviteCount: "${INVITE_COUNT}"  // numbers
 }
