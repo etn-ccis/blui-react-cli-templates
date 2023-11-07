e22978286d5139d61de1c69471525e96712adad2
jeffgreiner-eaton <jeffvgreiner@eaton.com>
2023-11-07 08:17:56 -0500

    add router main

--- authentication-typescript/template/src/router/main.tsx
+++ authentication-typescript/template/src/router/main.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, Navigate } from 'react-router-dom';
 import { PAGES } from './routes';
 
+
 export const MainRouter = (
     <>
         {PAGES.map((page) => {
