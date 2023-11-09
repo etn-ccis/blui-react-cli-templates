7c75df20701c21b3b9545b4460ac9c61659de3b7
Suraj Karambe <surajakarambe@eaton.com>
2023-11-09 15:27:41 +0530

    Re added test for auth

---
+++ authentication-typescript/template/src/App.test.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { ThemeProvider, createTheme, StyledEngineProvider } from '@mui/material/styles';
+import * as BLUIThemes from '@brightlayer-ui/react-themes';
+import { HomePage } from './pages/home';
+import { DrawerContext } from './contexts/drawerContextProvider';
+
+jest.mock('@brightlayer-ui/react-auth-shared', () => ({
+    useSecurityActions: (): any => ({
+        onUserNotAuthenticated: jest.fn(),
+        showChangePassword: jest.fn(),
+    }),
+}));
+
+test('renders welcome text', () => {
+    render(
+        <StyledEngineProvider injectFirst>
+            <ThemeProvider theme={createTheme(BLUIThemes.blue)}>
+                <DrawerContext.Provider
+                    value={{
+                        drawerOpen: true,
+                        setDrawerOpen: jest.fn(),
+                    }}
+                >
+                    <HomePage />
+                </DrawerContext.Provider>
+            </ThemeProvider>
+        </StyledEngineProvider>
+    );
+    const bluiText = screen.getByText(/Welcome to Brightlayer/i);
+    expect(bluiText).toBeInTheDocument();
+});
